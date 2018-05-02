bf635f20c36a7e4da7bb28b9826e08eb87baca6c
chenlingxi@delta2 <chanlingxi@gmail.com>
2018-05-02 12:55:42 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+
+
+ln -s ~/.rc/vimrc ~/.vimrc
+ln -s ~/.rc/tmux.conf ~/.tmux.conf
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+ln -s ~/.rc/UltiSnips ~/.vim/UltiSnips
+
+
+echo source ~/.rc/basic_bash.sh >> ~/.bashrc
+echo source ~/.rc/cityU_bash.sh >> ~/.bashrc
+echo source ~/.rc/cslab_bash.sh >> ~/.bashrc
+
+git config --global user.email "chanlingxi@gmail.com"
+git config --global user.name "chenlingxi@delta2"
+
+
+source ~/.bashrc
